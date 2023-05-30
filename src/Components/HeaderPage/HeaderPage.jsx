b0c73dea380b09386237f543db7f20a887a42b09
Wongthawat <89896981+Wongthawat@users.noreply.github.com>
2023-05-30 18:02:19 +0700

    update

--- src/Components/HeaderPage/HeaderPage.jsx
+++ src/Components/HeaderPage/HeaderPage.jsx
@@ -20,6 +20,9 @@ const HeaderPage = () => {
                 <img src="/src/assets/logo-w.webp" alt="logoJIB" className="ImgLogo" />
             </a>
         </div>
+        <div>
+            {Roles.role}
+        </div>
         <div className="col-lg-1 item-logout">
           {!token ? <></> : <a onClick={handleRemove}>Logout</a>}
         </div>
