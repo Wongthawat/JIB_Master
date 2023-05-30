455fdb1761125be3ecc0452ffede68b3ee27391f
Wongthawat <89896981+Wongthawat@users.noreply.github.com>
2023-05-30 12:34:30 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
