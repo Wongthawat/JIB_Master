3caca4fea130c9376e9e3f311df90bd606542b1b
Wongthawat <89896981+Wongthawat@users.noreply.github.com>
2023-06-01 14:51:59 +0700

    update

--- src/Pages/BannerPage/BannerPage.jsx
+++ src/Pages/BannerPage/BannerPage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Grid from "../../Components/Grid/Grid";
+import Card from "@mui/material/Card";
+import axios from "axios";
 
 const BannerPage = () => {
-  return (
-    <div>BannerPage</div>
-  )
-}
+  const URL = "https://www.melivecode.com/api";
+  const [items, setItems] = useState([]);
 
-export default BannerPage
+  const GetData = async () => {
+    try {
+      const res = await axios.get(`${URL}` + "/users");
+      if (res.status === 200) {
+        setItems(res?.data);
+        console.log(res?.data);
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    GetData();
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "fname", headerName: "Username", width: 200, editable: true },
+    { field: "lname", headerName: "Lastname", width: 200, editable: true },
+    { field: "username", headerName: "Email", width: 200, editable: true },
+    {
+      field: "avatar",
+      headerName: "Profile",
+      width: 100,
+      renderCell: (params) => <img src={params.value} alt="" className="w-100" />,
+    },
+  ];
+
+  return (
+    <>
+      <div>
+        <Card>
+          <Grid Items={items} Columns={columns} />
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default BannerPage;
